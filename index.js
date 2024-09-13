46766e9ec7417a2f6f8f4ce4de83783b392bdff9
Alena <92329824+poletala@users.noreply.github.com>
2024-09-14 00:30:13 +0500

    first version with validation

---
+++ index.js
@@ -0,0 +1,87 @@
+const nameInput = document.querySelector('#name')
+const phoneInput = document.querySelector('#phone')
+const messageInput = document.querySelector('#message')
+const theme = document.querySelector('#theme')
+const form = document.querySelector('form')
+const inputElements = [nameInput, phoneInput, messageInput]
+const nameRegex = /^[a-zA-Zа-яА]+$/;
+const phoneRegex = /(^(?!\+.*\(.*\).*--.*$)(?!\+.*\(.*\).*-$)(([0-9]{0,12})?(\+[0-9]{11})?)$)|(^.*$)/; 
+
+function validateForm() {
+    const name = nameInput.value.trim()
+    const phone = phoneInput.value.trim().replace(/[- )( +]/g,'')
+    let errors = []
+    inputElements.map(function(element) {
+
+        const elementValue = element.value.trim()
+        
+        if (!elementValue) {
+            element.classList.add('error-style')
+            element.nextElementSibling.classList.remove('invalid')
+            element.nextElementSibling.innerHTML = 'Поле не должно быть пустым'
+            errors.push(element)
+        }
+        if (elementValue && element.name === 'name' && !nameRegex.test(name)) {
+            element.classList.add('error-style')
+            element.nextElementSibling.classList.remove('invalid')
+            element.nextElementSibling.innerHTML = 'Имя может содержать только буквы'
+            errors.push(element)
+        }
+        if (elementValue && element.name === 'phone' && (!phoneRegex.test(phone) || phone.length !== 11)) {
+            element.classList.add('error-style')
+            element.nextElementSibling.classList.remove('invalid')
+            element.nextElementSibling.innerHTML = 'Неверный формат номера'
+            errors.push(element)
+        }
+        return errors
+    })
+
+    if (errors.length) {
+        return false
+    } else {
+        return true
+    }
+}
+
+function onSubmit () {
+    if (validateForm()) {
+        let userMessage = {
+            name: nameInput.value,
+            phone: phoneInput.value,
+            theme: theme.value,
+            message: messageInput.value
+        }
+        console.log(userMessage)
+        clearFields ()
+        alert('Сообщение отправлено!')
+    } else {
+        alert('Обнаружены неверные данные. Проверьте правильность введенных полей.')
+    }
+    
+}
+
+function deleteErrorsStyle(elem) {
+    if (!elem.nextElementSibling.classList.contains('invalid')) {
+        elem.nextElementSibling.classList.add('invalid')
+        elem.nextElementSibling.innerHTML = ''
+        elem.classList.remove('error-style')
+    }
+}
+
+function clearFields () {
+    nameInput.value = ''
+    phoneInput.value = ''
+    messageInput.value = ''
+}
+
+function handleFormSubmit(event) {
+    event.preventDefault()
+    onSubmit() 
+  }
+  
+form.addEventListener('submit', handleFormSubmit)
+
+
+
+
+
